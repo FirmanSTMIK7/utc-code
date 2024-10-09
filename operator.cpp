4ea2a4c8b4a9015eefb495f0a8da51587a103543
FirmanSTMIK7 <firmanmaulanastmik@gmail.com>
2024-10-10 06:45:05 +0800

    file operator c++

---
+++ operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    
+    cout << "nilai a : ";
+    cin >> a;
+    cout << "nilai b : ";
+    cin >> b;
+    cout << "nilai a sama dengan b = " << (a == b) << endl;
+    cout << "nilai a lebih kecil dari b = " << (a <= b) << endl;
+    cout << "nilai a lebih besar dari b = " << (a >= b) << endl;
+    cout << "nilai tidak sama dengan b = " << (a != b) << endl;
+   
+}
